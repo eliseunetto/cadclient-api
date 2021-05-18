bb21c3712d6a70ec448c936e516321f2542832c2
eliseunetto <eliseunetto@hotmail.com>
2021-05-18 13:43:36 -0300

    Project created

---
+++ src/main/java/com/eliseunetto/cadclient/CadclientApiApplication.java
@@ -0,0 +1,13 @@
+package com.eliseunetto.cadclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadclientApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadclientApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliseunetto/cadclient/CadclientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliseunetto.cadclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadclientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
