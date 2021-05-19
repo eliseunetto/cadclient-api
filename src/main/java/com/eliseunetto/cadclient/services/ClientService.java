162a74a27a4cc7305c8fd9fa5385425b1661df5a
eliseunetto <eliseunetto@hotmail.com>
2021-05-19 00:10:41 -0300

    Exception Update

--- src/main/java/com/eliseunetto/cadclient/services/ClientService.java
+++ src/main/java/com/eliseunetto/cadclient/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -41,10 +43,14 @@ public class ClientService {
 	
 	@Transactional
 	public ClientDTO update(Long id, ClientDTO dto) {
-		Client entity = repository.getOne(id);
-		copyDtoToEntity(dto, entity);
-		entity = repository.save(entity);
-		return new ClientDTO(entity);
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found");
+		}
 	}
 
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
