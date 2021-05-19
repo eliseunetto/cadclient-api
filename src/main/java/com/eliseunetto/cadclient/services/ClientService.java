7cc9c259455df17d21c202ea3fa992550a02266c
eliseunetto <eliseunetto@hotmail.com>
2021-05-18 21:03:59 -0300

    Client repository, service, resource and GET

---
+++ src/main/java/com/eliseunetto/cadclient/services/ClientService.java
@@ -0,0 +1,30 @@
+package com.eliseunetto.cadclient.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.eliseunetto.cadclient.entities.Client;
+import com.eliseunetto.cadclient.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Client> findAll() {
+		List<Client> list = repository.findAll();
+		return list;
+	}
+	
+	@Transactional(readOnly = true)
+	public Client findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
